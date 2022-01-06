4da6e8ccc21c4387fcfe68250fab842efea553a8
Emir Hasan Işık <emir.h.isik@gmail.com>
2022-01-06 18:06:46 +0300

    Day Isolation End

---
+++ ProgrammersBlog.Entities/Dtos/CategoryAddDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+    public class CategoryAddDto
+    {
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage = "{0} boş bırakılamaz.")]
+        [MaxLength(70,ErrorMessage = "{0} {1} karakterden uzun olamaz.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden kısa olamaz.")]
+        public string Name { get; set; }
+        [DisplayName("Kategori Açıklaması")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden uzun olamaz.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden kısa olamaz.")]
+        public string Description { get; set; }
+        [DisplayName("Kategori Notu")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden uzun olamaz.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden kısa olamaz.")]
+        public string Note { get; set; }
+        [DisplayName("Kategori Aktifliği")]
+        [Required(ErrorMessage = "{0} boş bırakılamaz.")]
+        public bool IsActive { get; set; }
+    }
+}
---
+++ ProgrammersBlog.Services/Abstract/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+using System.Threading.Tasks;
+using ProgrammersBlog.Entities.Concrete;
+using System.Collections.Generic;
+using ProgrammersBlog.Entities.Dtos;
+
+namespace ProgrammersBlog.Services.Abstract
+{
+    public interface ICategoryService
+    {
+        Task<IDataResult<Category>> Get(int categoryId);
+        Task<IDataResult<IList<Category>>> GetAll();
+        Task<IResult> Add(CategoryAddDto categoryAddDto, string createdByName);
+        Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int categoryId);
+        Task<IResult> HardDelete(int categoryId);
+    }
+}
