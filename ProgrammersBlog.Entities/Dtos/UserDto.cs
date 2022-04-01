c9f05ebd5b4dbbdae58234f41aa5b9fb1460d40c
emirhisik <91196254+emirhisik@users.noreply.github.com>
2022-04-01 17:30:07 +0300

    Day 111

---
+++ ProgrammersBlog.Entities/Dtos/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+    public class UserDto:DtoGetBase
+    {
+        public UserAddDto User { get; set; }
+    }
+}
